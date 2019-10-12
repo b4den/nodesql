d89d7dd081987bf30c6fec19d5c249644c214c4c
Baden Delamore <52495447+b4den@users.noreply.github.com>
2019-10-12 21:03:33 +0100

    Big refactor

--- app/index.js
+++ app/index.js
@@ -1,25 +1,24 @@
-var express = require("express");
-var body_parser = require("body-parser")
-var app = express();
-var router = express.Router();
-require('dotenv').config();
+const express = require('express');
+const body_parser = require('body-parser');
 
-app.use('/',router);
+const app = express();
+const router = express.Router();
+
+app.use('/', router);
 app.use(express.static(__dirname + '/public'));
 
-router.all('*', (req, res, next) =>  {
+router.all('*', (req, res, next) => {
   res.header('Access-Control-Allow-Origin', '*');
   res.header('Access-Control-Allow-Headers', 'X-Requested-With');
   res.header('Access-Control-Allow-Headers', 'Content-Type');
   next();
 });
 
-router.use(body_parser.urlencoded({ extended : true }))
-router.use(body_parser.json())
+router.use(body_parser.urlencoded({ extended: true }));
+router.use(body_parser.json());
 
-var port = process.env.PORT || 3000;
-app.listen(port, function() {
-  console.log("Listening on " + port);
+const port = process.env.PORT || 3000;
+app.listen(port, () => {
+  console.log(`Listening on  ${port}`);
 });
-require('./routes')(router)
-
+require('./routes')(router);
--- app/routes/index.js
+++ app/routes/index.js
@@ -1,10 +1,12 @@
-var fs = require('fs');
-var path = require('path');
-var { 
+const fs = require('fs');
+const path = require('path');
+
+const {
   get_databases,
   get_tables,
   get_columns,
-  get_dump } = require(__dirname + '/../models/injection_model');
+  get_dump,
+}  = require(`${__dirname}/../models/injection_model`);
 
 
 module.exports = (router) => {
@@ -12,65 +14,61 @@ module.exports = (router) => {
   res.sendFile(path.join(__dirname + '/../layout.html'));
   });
 
-  router.post('/api/database', async (req, res) => { 
+  router.post('/api/database', async (req, res) => {
     const { url } = req.body
 
     try {
-      let db_info = await get_databases(url)
-      res.send({"data":db_info})
-    }
-    catch (err) {
-      console.log(err)
-      res.status(401).send({error: err})
+      const db_info = await get_databases(url);
+      res.send({ data: db_info });
+    } catch (err) {
+      console.log(err);
+      res.status(401).send({ error: err });
     }
   });
 
-  router.post('/api/tables', async (req, res) => { 
-    const { url, database } = req.body
+  router.post('/api/tables', async (req, res) => {
+    const { url, database } = req.body;
 
     try {
-      let db_info = await get_tables(url, database)
-      res.send({"data":db_info})
-    }
-    catch (err) {
-      console.log(err)
-      res.status(401).send({error: err})
+      const db_info = await get_tables(url, database);
+      res.send({ data: db_info });
+    } catch (err) {
+      console.log(err);
+      res.status(401).send({ error: err });
     }
   });
 
-  router.post('/api/columns', async (req, res) => { 
-    const { url, database, table } = req.body
+  router.post('/api/columns', async (req, res) => {
+    const { url, database, table } = req.body;
 
     try {
-      let db_info = await get_columns(url, database, table)
-      res.send({"data":db_info})
-    }
-    catch (err) {
-      console.log(err)
-      res.status(401).send({error: err})
+      const db_info = await get_columns(url, database, table);
+      res.send({ data: db_info });
+    } catch (err) {
+      console.log(err);
+      res.status(401).send({ error: err });
     }
   });
 
-  router.post('/api/dump', async (req, res) => { 
-    const { url, database, table } = req.body
+  router.post('/api/dump', async (req, res) => {
+    const { url, database, table } = req.body;
 
     try {
-      let db_info = await get_dump(url, database, table)
-      res.send({"data":db_info})
-    }
-    catch (err) {
-      console.log(err)
-      res.status(401).send({error: err})
+      const db_info = await get_dump(url, database, table);
+      res.send({ data: db_info });
+    } catch (err) {
+      console.log(err);
+      res.status(401).send({ error: err });
     }
   });
 
 
-    /* simple method to require all routes in the routes directory, while passing
-    * references to the required imports */
+  /* simple method to require all routes in the routes directory, while passing
+   * references to the required imports */
   fs.readdirSync(__dirname).forEach((file) => {
-    if (file == "index.js") return;
-    if (path.extname(file) != ".js") return;
-    var name = file.substr(0, file.indexOf('.'));
+    if (file === 'index.js') return;
+    if (path.extname(file) !== '.js') return;
+    const name = file.substr(0, file.indexOf('.'));
     require('./' + name)(router);
   });
-}
+};
